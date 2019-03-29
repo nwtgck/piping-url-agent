6a5286fb1f7ca74d3b8f1f210df3aa545801e0ac
Ryo Ota <nwtgck@gmail.com>
2019-03-29 11:14:19 +0900

    [add] Inject HTTP server port by command-line option

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,17 @@
 import {generateHandler} from "./piping-url-agent";
 import * as http from "http";
+import * as yargs from "yargs";
 
-// TODO: hard code
-const httpPort = 3000;
+// Option parser
+const parser = yargs
+  .option("http-port", {
+    describe: "HTTP port",
+    default: 3000
+  });
+
+const args = parser.parse(process.argv);
+
+const httpPort: number = args["http-port"];
 
 // Ignore SSL certificate for Piping Server
 // (from: https://stackoverflow.com/a/21961005/2885946)
